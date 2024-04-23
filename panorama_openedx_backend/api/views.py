0541819580f682152a68aa12f6523be57286583d
lkatsikaris <lkatsikaris@aulasneo.com>
2024-04-23 15:56:15 -0300

    user arn and dashboard type taken from env settings

--- panorama_openedx_backend/api/views.py
+++ panorama_openedx_backend/api/views.py
@@ -14,53 +14,35 @@ class GetDashboardEmbedUrl(APIView):
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
         )
 
-        # basic_dashboard_id
-        enrollments_dashboard_id = "781857d0-dc98-47f7-bbaf-153c0a252e78"
-        grading_dashboard_id = "8f8cfbe8-62d7-4af2-8c3f-0b7871a77078"
-        user_activity_dashboard_id = "67adb477-ec5d-4dad-b506-85a7c91b9095"
-        engagement_dashboard_id = "d030623c-2fe2-4095-80ce-8ec819c70ff1"
-
-        dashboardTypes = {
-            "basic": [
-                {
-                    "name": "enrollments",
-                    "displayName": "Enrollments",
-                    "id": enrollments_dashboard_id,
-                    "url": ""
-                },
-                {
-                    "name": "engagement",
-                    "displayName": "Engagement",
-                    "id": engagement_dashboard_id,
-                    "url": ""
-                },
-                {
-                    "name": "user activity",
-                    "displayName": "User Activity",
-                    "id": user_activity_dashboard_id,
-                    "url": ""
-                },
-                {
-                    "name": "grading",
-                    "displayName": "Grading",
-                    "id": grading_dashboard_id,
-                    "url": ""
-                }
-            ]
-        }
-
         quicksight = session.client(
             "quicksight",
-            region_name='us-west-2',
+            region_name=settings.PANORAMA_REGION,
         )
 
         user = request.user
 
-        for dashboard in dashboardTypes["basic"]:
+        user_meta = json.loads(user.profile.meta)
+
+
+        # CHECKING IF USER HAS AN ARN SET
+        if user_meta.get("panorama_user_arn"):
+            quicksightARN = user_meta.get("panorama_user_arn")
+        else:
+            raise ValueError('Error 403 - Forbidden')
+        
+        # CHECKING IF USER HAS A DASHBOARD TYPE SET
+        if user_meta.get("dashboard_type"):
+            dashboard_type_of_user = user_meta.get("dashboard_type")
+        else:
+            raise ValueError('Error 404 - Dashboard not assigned')
+
+        all_dashboards = settings.PANORAMA_DASHBOARD_TYPES
+
+        for dashboard in all_dashboards[dashboard_type_of_user]:
             response = quicksight.generate_embed_url_for_registered_user(
-                AwsAccountId='212507341974',
+                AwsAccountId=settings.PANORAMA_AWS_ACCOUNT_ID,
                 SessionLifetimeInMinutes=123,
-                UserArn='arn:aws:quicksight:us-west-2:212507341974:user/default/andres',
+                UserArn=quicksightARN,
                 ExperienceConfiguration={
                     'Dashboard': {
                         'InitialDashboardId': dashboard['id'],
@@ -69,12 +51,7 @@ class GetDashboardEmbedUrl(APIView):
             )
             dashboard['url'] = response['EmbedUrl']
 
-        data = {
-            'embed_url': user.profile.meta,
-            'email': user.email,
-        }
-
         return Response({
             'statusCode': 200,
-            'body': dashboardTypes["basic"]
+            'body': all_dashboards[dashboard_type_of_user]
         })
